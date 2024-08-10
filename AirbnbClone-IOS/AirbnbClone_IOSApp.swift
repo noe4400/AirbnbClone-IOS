1e41bcce518f5d095dfdc3917ce4a19e862fe5d0
fgarcia <gfabian757@gmail.com>
2024-08-09 20:46:13 -0600

    Initial Commit

---
+++ AirbnbClone-IOS/AirbnbClone_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbClone_IOSApp.swift
+//  AirbnbClone-IOS
+//
+//  Created by Fabian Garcia on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbClone_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbnbClone-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirbnbClone-IOS
+//
+//  Created by Fabian Garcia on 09/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
