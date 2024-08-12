2191ff8fe89438b0f4dee961334175045242d80a
fgarcia <gfabian757@gmail.com>
2024-08-11 18:15:58 -0600

    refactor(NetworkManager) refactor code to avoid duplicate code

--- AirbnbClone-IOS/Services/NetworkManager.swift
+++ AirbnbClone-IOS/Services/NetworkManager.swift
@@ -13,24 +13,26 @@ class NetworkManager: NSObject {
     init(session: URLSession = .shared) {
         self.session = session
     }
-
-    func getListings(
-        completed: @escaping (Result<[ListingDataModel], NetworkError>) -> Void
+    
+    func featchData<T: Decodable>(
+        resourceName: String,
+        as type: T.Type,
+        completed: @escaping (Result<T, NetworkError>) -> Void
     ) {
-        guard
-            let url = Bundle.main.url(
-                forResource: "TAB_677", withExtension: "json")
-        else {
+        guard let url = Bundle.main.url(forResource: resourceName, withExtension: "json") else {
             completed(.failure(.invalidURL))
             return
         }
-        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {
-            data, response, error in
-
+        print("url", url)
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            
             if error != nil {
+                print("error nil")
                 completed(.failure(.unableToComplete))
                 return
             }
+            
             // comment out this part as this request comes from a json file wil be use when api is integrated
             //            guard let response = response as? HTTPURLResponse,
             //                response.statusCode == 200
@@ -38,26 +40,35 @@ class NetworkManager: NSObject {
             //                completed(.failure(.invalidResponse))
             //                return
             //            }
-
+            
             guard let data = data else {
+                print("failing data")
                 completed(.failure(.invalidData))
                 return
             }
 
             do {
                 let decoder = JSONDecoder()
-                let decodedResponse = try decoder.decode(
-                    [ListingDataModel].self, from: data)
+                let decodedResponse = try decoder.decode(T.self, from: data)
                 completed(.success(decodedResponse))
             } catch {
                 print("invalid data from catch", error)
                 completed(.failure(.invalidData))
             }
-
         }
-
+        
         task.resume()
     }
+    
+
+    func getListings(completed: @escaping (Result<[ListingDataModel], NetworkError>) -> Void) {
+        featchData(resourceName: "TAB_677", as: [ListingDataModel].self, completed: completed)
+    }
+    
+    func getCategories(completed: @escaping (Result<[CategoryModel], NetworkError>) -> Void) {
+        featchData(resourceName: "categories", as: [CategoryModel].self, completed: completed)
+    }
+    
 }
 
 enum NetworkError: Error {
